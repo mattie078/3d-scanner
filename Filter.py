bcf353e5002202728e058ae838ec31c22d63978f
Ismahaan Mohamed <IsmahaanMohamed@outlook.com>
2019-09-25 14:21:12 +0200

    red detection

---
+++ Filter.py
@@ -0,0 +1,20 @@
+import numpy as np
+from cv2 import cv2
+
+
+
+image = cv2.imread("test.jpg")
+
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+ 
+lower_red = np.array([150,150,0])
+upper_red = np.array([255,255,255])
+
+mask = cv2.inRange(hsv,lower_red,upper_red)
+
+cv2.imshow("original image", image)
+
+cv2.imshow("filter",mask)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
