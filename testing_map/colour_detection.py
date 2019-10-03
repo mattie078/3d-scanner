1daf4010d46084439424bf8cae6eaceb1a99628b
Matthijs Meijerink <0962038@hr.nl>
2019-10-03 14:48:52 +0200

    werkt niet

--- testing_map/colour_detection.py
+++ testing_map/colour_detection.py
@@ -7,29 +7,6 @@ class getContours(object):
     
     def __init__(self):
 
-        # Get image
-        #self.path = r'test.jpg'
-        #self.frame = cv2.imread(self.path)
-
-        # # converting from BGR to HSV color space
-        # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        # # Range
-	#self.lower = np.array([0,50,50])
-	#self.upper = np.array([10,255,255])
-            
-        # mask_before = cv2.inRange(hsv, lower_red, upper_red)
-        # mask_after = cv2.bitwise_not(mask_before)
- 
-        # res1 = cv2.bitwise_and(frame,frame,mask=mask_before)
-        # res2 = cv2.bitwise_and(frame,frame,mask=mask_after)
-
-        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-        # gray = cv2.GaussianBlur(gray, (5, 5), 0) 
-        # thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1] 
-        # thresh = cv2.erode(thresh, None, iterations=2) 
-        # thresh = cv2.dilate(thresh, None, iterations=2)
-
         self.X=[]
         self.Y=[]
         self.redlow=150
@@ -41,42 +18,44 @@ class getContours(object):
 
     def calculateContour(self):
 
-        #VICEOCAPTURE INPUT
-        self.cap = cv2.VideoCapture(0) #Video Device(webcam) is opened
-        self.frame = self.cap.read()[1]
-        self.hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+        #Get image
+        self.path = r'test.jpg'
+        self.frame = cv2.imread(self.path)
 
+        # converting from BGR to HSV color space
+        self.hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+        cv2.imshow("hsv",self.hsv)
+
+        # Range
         self.lower = np.array([0,50,50])
         self.upper = np.array([10,255,255])
-
-        self.mask = cv2.inRange(self.frame, self.lower, self.upper) 
-
-        self.mask_before = cv2.inRange(self.hsv, self.lower, self.upper)
-        self.mask_after = cv2.bitwise_not(self.mask_before)
+            
+        mask_before = cv2.inRange(self.hsv, self.lower, self.upper)
+        mask_after = cv2.bitwise_not(mask_before)
  
-        self.res1 = cv2.bitwise_and(self.frame,self.frame,mask=self.mask_before)
-        self.res2 = cv2.bitwise_and(self.frame,self.frame,mask=self.mask_after)
+        res1 = cv2.bitwise_and(self.frame,self.frame,mask=mask_before)
+        res2 = cv2.bitwise_and(self.frame,self.frame,mask=mask_after)
 
-        self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY) 
-        self.gray = cv2.GaussianBlur(self.gray, (5, 5), 0) 
-        self.thresh = cv2.threshold(self.gray, 45, 255, cv2.THRESH_BINARY)[1] 
-        self.thresh = cv2.erode(self.thresh, None, iterations=2) 
-        self.thresh = cv2.dilate(self.thresh, None, iterations=2)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+        gray = cv2.GaussianBlur(gray, (5, 5), 0) 
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1] 
+        thresh = cv2.erode(thresh, None, iterations=2) 
+        thresh = cv2.dilate(thresh, None, iterations=2)
     
 	    #Finding the contours
-        self.cnts = cv2.findContours(self.thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        self.cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         self.cnts = self.cnts[0] if imutils.is_cv2() else self.cnts[1]
 
         for i in range(len(self.cnts)):
             self.c=self.cnts[i]
-            cv2.drawContours(self.frame, [self.c], -1, (0, 255, 255), 2)
+            cv2.drawContours(res2, [self.c], -1, (0, 255, 255), 2)
 
         # cv2.imshow("original",frame)
-        cv2.imwrite("output.jpg", self.frame) 
+        cv2.imwrite("output.jpg", res2) 
         # cv2.imshow("result1",self.output_img)
         # cv2.imshow("thresh",thresh)
         # cv2.imshow("result2",res2)
-	cv2.waitKey(0)    
+   
 def main():
     my_obj = getContours()
     my_obj.calculateContour() 
