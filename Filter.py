9ccb1501267acb305c6d80c3d5eceb5cfbc7eeda
Herman Heringa <hermanheringa@hotmail.com>
2019-10-07 11:44:45 +0200

    test

--- Filter.py
+++ Filter.py
@@ -4,13 +4,12 @@ import cv2
 class Filter:
     def colorDetection(self, m):
         image = cv2.imread('/home/pi/Desktop/Fotos/test' + str(m) + '.jpg')
-        
+
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
          
         lower_red = np.array([150,150,80])
         upper_red = np.array([255,255,255])
 
         mask = cv2.inRange(hsv,lower_red,upper_red)
-        
-        
-        cv2.imwrite('/home/pi/Desktop/FotosHSV/filer' +str(m)+'.jpg', mask)
+
+        cv2.imwrite('FotosHSV/filer' +str(m)+'.jpg', mask)
