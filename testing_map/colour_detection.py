cf96d32bc7142b342f5332548fb5dae14319e52c
Matthijs Meijerink <0962038@hr.nl>
2019-10-07 09:52:25 +0200

    DWAd

--- testing_map/colour_detection.py
+++ testing_map/colour_detection.py
@@ -22,7 +22,7 @@ class getContours(object):
         self.path = r'test.jpg'
         #self.frame = cv2.imread(self.path) #Read the video device input
 		#self.frame = cv2.flip(self.frame, 1) #This should be uncommented to get the mirror image of the actual frame
-        self.frame = self.cap.read()
+        self.frame = self.cap.read()[1]
         self.lower = np.array([self.bluelow,self.greenlow,self.redlow]) #lower limit of BGR values of the laser line
         self.upper= np.array([self.blueup,self.greenup,self.redup]) #upper limit of BGR values of the laser line
         self.mask = cv2.inRange(self.frame, self.lower, self.upper) #create a mask within the specified values of RED
