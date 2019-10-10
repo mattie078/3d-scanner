d460facc428b27852b2becda4b2171acddb44b17
Herman Heringa <hermanheringa@hotmail.com>
2019-10-10 16:26:27 +0200

    duplicates removed

--- test_camera.py
+++ test_camera.py
@@ -7,20 +7,23 @@ image = cv2.imread(path)
 
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
          
-lower_red = np.array([150,150,80])
-upper_red = np.array([255,255,255])
+lower_red = np.array([150, 150, 80])
+upper_red = np.array([255, 255, 255])
 
-mask = cv2.inRange(hsv,lower_red,upper_red)
+mask = cv2.inRange(hsv, lower_red, upper_red)
 
 new_image = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 print(new_image.shape)
 
-sought = [255,255,255]
-
 indices = np.where(new_image == [255])
 coordinates = list(zip(indices[0], indices[1]))
+coordinatesNoDupes = []
+for index in coordinates:
+    if index not in coordinatesNoDupes:
+        coordinatesNoDupes.append(index)
 
-zipped_list = coordinates[:]
+zipped_list = coordinatesNoDupes[:]
+print(zipped_list)
 print(len(zipped_list))
 
 #result = np.count_nonzero(np.all(new_image==sought,axis=2))
