7323305ad87fd490363b0930d552dcf1b49d612c
Matthijs Meijerink <0962038@hr.nl>
2019-10-16 11:11:24 +0200

    New test directory

---
+++ testing_pointcloud/Main.py
@@ -0,0 +1,12 @@
+from Coordinates import Coordinates
+from Intergration import Intergration
+
+def main():
+    Coordinates.calculateCoordinates()
+
+    Temp = Intergration()
+    Intergration.ReadFile(Temp)
+    Intergration.CalculateXYZ(Temp)
+
+if __name__ == "__main__":
+    main()
