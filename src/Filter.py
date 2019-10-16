007aaa30cf641a715b8fc075a8d5d42c6e0d04b4
Herman Heringa <hermanheringa@hotmail.com>
2019-10-16 12:45:27 +0200

    Revert "nieuw filter werkend"
    This reverts commit 07a488c5d4652c5f5fb675c9e25e1e7df5854f26.

--- src/Filter.py
+++ src/Filter.py
@@ -2,16 +2,16 @@ import numpy as np
 import cv2
 
 class Filter:
-#    def colorDetection(self, m):
-	 for n in range(0,72):
-		image = cv2.imread('/home/pi/Desktop/3d-scanner/Fotos/test' + str(n) + '.jpg')
-        	
-		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-         	
-		lower_red = np.array([253,245,245])
-		upper_red = np.array([255,255,255])
+    def colorDetection(self, m):
+        image = cv2.imread('/home/pi/Desktop/3d-scanner/Fotos/test' + str(m) + '.jpg')
+        
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+         
+        lower_red = np.array([253,245,245])
+        upper_red = np.array([255,255,255])
 
-		mask = cv2.inRange(hsv,lower_red,upper_red)
-        	
-        	
-		cv2.imwrite('/home/pi/Desktop/3d-scanner/FotosHSV/filter' +str(n)+'.jpg', mask)
+        mask = cv2.inRange(hsv,lower_red,upper_red)
+        
+        
+        cv2.imwrite('/home/pi/Desktop/3d-scanner/FotosHSV/filter' +str(m)+'.jpg', mask)
+
