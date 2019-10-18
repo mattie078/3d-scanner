0f3aeae1209d170b7dea319c187a4feb8ef88157
mattie078 <matthijsmeijerink97@gmail.com>
2019-10-18 14:33:15 +0200

    test3

--- src/kalibratie.py
+++ src/kalibratie.py
@@ -2,5 +2,5 @@ from picamera import PiCamera
 import time
 
 camera = PiCamera()
-camera.capture('/home/pi/Desktop/3d-scanner/Fotos/afstandtest2.jpg')
+camera.capture('/home/pi/Desktop/3d-scanner/Fotos/afstandtest3.jpg')
 time.sleep(1)
