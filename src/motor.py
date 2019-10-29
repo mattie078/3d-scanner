581a16184a7b0b9e9349f61bdd5678abe6661d28
Matthijs Meijerink <0962038@hr.nl>
2019-10-29 10:59:56 +0100

    Added some comments

--- src/motor.py
+++ src/motor.py
@@ -6,6 +6,7 @@ from Filter import Filter
 
 class Motor:
     try:
+        # Opent de camera om een foto te maken
         camera = PiCamera()
         GPIO.setmode(GPIO.BCM)
         ControlPin = [6,13,19,26]
@@ -14,6 +15,7 @@ class Motor:
             GPIO.setup(pin,GPIO.OUT)
             GPIO.output(pin,0)
 
+        # Sequence om de motor juist te laten draaien
         seq = [[1,0,0,0],
                [1,1,0,0],
                [0,1,0,0],
@@ -23,8 +25,8 @@ class Motor:
                [0,0,0,1],
                [1,0,0,1],
                [1,0,0,0]]
-        #ismahaan!!!!!!!!
-        
+
+        # Maakt 73 fotos (0 tot 72) en slaat deze op met de juiste benaming
         for m in range(73):
             fotonaam = 'test' + str(m) + ".jpg"
             camera.capture('/home/pi/Desktop/3d-scanner/Fotos/'+fotonaam)
@@ -35,7 +37,6 @@ class Motor:
                     for pin in range(4):
                         GPIO.output(ControlPin[pin], seq[halfstep][pin])
                         time.sleep(0.001)
-            #print("DRAAIEN!!!!")
             time.sleep(1)
                     
     #except:
