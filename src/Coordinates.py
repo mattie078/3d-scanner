3d1907272a81469a028cddf1b7856b9d284ad686
Herman Heringa <hermanheringa@hotmail.com>
2019-11-01 11:28:15 +0100

    Coordinates verbeterd

--- src/Coordinates.py
+++ src/Coordinates.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 
-class Coordinates():
-    
-    def calculateCoordinates():
+
+class Coordinates:
+
+    def calculateCoordinates(self):
         for i in range(73):
             # Pakt de fotos die net zijn gemaakt
             path = "Fotos/filter" + str(i) + ".jpg"
@@ -20,4 +21,4 @@ class Coordinates():
                     coordinatesNoDupes.append(index)
 
             zipped_list = coordinatesNoDupes[:]
-            np.savetxt('xycoordinates.txt', zipped_list, fmt = '%d')
+            np.savetxt('yxcoordinates' + str(i) + '.txt', zipped_list, fmt = '%d')
