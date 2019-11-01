3e1784643b2176df3fddad16eb0612375412a9b9
Matthijs Meijerink <0962038@hr.nl>
2019-11-01 10:48:18 +0100

    Changed Coordinates.py to loop through all filter photos

--- src/Coordinates.py
+++ src/Coordinates.py
@@ -4,31 +4,20 @@ import numpy as np
 class Coordinates():
     
     def calculateCoordinates():
-        # Pakt de fotos die net zijn gemaakt
-        path = r'test.jpg'
-        image = cv2.imread(path)
+        for i in range(73):
+            # Pakt de fotos die net zijn gemaakt
+            path = "Fotos/filter" + str(i) + ".jpg"
+            new_image = cv2.imread(path)
 
-        # Zet de fotos om in HSV formaat voor verdere bewerking
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-         
-        lower_red = np.array([150, 150, 80])
-        upper_red = np.array([255, 255, 255])
+            # Kijkt en slaat de pixels op in xycoordinates die wit zijn 
+            indices = np.where(new_image == [255])
 
-        # Maakt een mask die tussen de 2 waardes hierboven zit
-        mask = cv2.inRange(hsv, lower_red, upper_red)
+            coordinates = list(zip(indices[0], indices[1]))
+            coordinatesNoDupes = []
 
-        # Voegt de mask toe aan de fotos. Nu moeten we alleen de rode gebieden zien
-        new_image = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+            for index in coordinates:
+                if index not in coordinatesNoDupes:
+                    coordinatesNoDupes.append(index)
 
-        # Kijkt en slaat de pixels op in xycoordinates die wit zijn 
-        indices = np.where(new_image == [255])
-
-        coordinates = list(zip(indices[0], indices[1]))
-        coordinatesNoDupes = []
-
-        for index in coordinates:
-            if index not in coordinatesNoDupes:
-                coordinatesNoDupes.append(index)
-
-        zipped_list = coordinatesNoDupes[:]
-        np.savetxt('xycoordinates.txt', zipped_list, fmt = '%d')
+            zipped_list = coordinatesNoDupes[:]
+            np.savetxt('xycoordinates.txt', zipped_list, fmt = '%d')
