bc8e9d26ada7dfe3e2bb56d03f4080ff76c60b4e
Herman Heringa <hermanheringa@hotmail.com>
2019-11-07 13:55:48 +0100

    Coordinates PEP8 friendly gemaakt

--- src/Coordinates.py
+++ src/Coordinates.py
@@ -6,10 +6,10 @@ import time
 
 class Coordinates:
 
-    def calculateCoordinates(self):
+    def calculate_coordinates(self):
         for i in range(73):
             # Pakt de fotos die net zijn gemaakt
-            path = "../FotosHSV/filter" + str(i) + ".jpg"
+            path = "..\\FotosHSV\\filter" + str(i) + ".jpg"
             new_image = cv2.imread(path)
 
             # Kijkt en slaat de pixels op in xycoordinates die wit zijn 
@@ -20,12 +20,12 @@ class Coordinates:
             #         print(indices[width][height])
 
             coordinates = list(zip(indices[0], indices[1]))
-            coordinatesNoDupes = []
+            coordinates_no_dupes = []
 
             for index in coordinates:
-                if index not in coordinatesNoDupes:
-                    coordinatesNoDupes.append(index)
-            zipped_list = coordinatesNoDupes[:]
+                if index not in coordinates_no_dupes:
+                    coordinates_no_dupes.append(index)
+            zipped_list = coordinates_no_dupes[:]
 
             df = pd.DataFrame(zipped_list, columns={'yVal', 'xVal'})
 
@@ -34,20 +34,19 @@ class Coordinates:
             df2 = df2.drop_duplicates()
             
             # This code gets all Y values from specific X value (X values are still in array type)
-            listWalker = 0
-            avgList = []
+            avg_list = []
 
-            for index_old, non_dublicated_yVal in df2.iteritems(): 
-                val = df.loc[df["xVal"] == non_dublicated_yVal, "yVal"]
+            for index_old, non_duplicated_yVal in df2.iteritems():
+                val = df.loc[df["xVal"] == non_duplicated_yVal, "yVal"]
 
                 # val.real returns array with values instead of an dataframe
                 val_np = np.array(val.real)
                 if val_np.size != 0:
 
                     length = val_np.max(axis=0)-val_np.min(axis=0)
-                    avgList.append([non_dublicated_yVal, length])
+                    avg_list.append([non_duplicated_yVal, length])
 
             print("Finished calc:"+str(i))
-            np.savetxt('..\calcs\\tempCalc' + str(i) + '.txt', avgList, fmt='%d')
+            np.savetxt('..\\calcs\\tempCalc' + str(i) + '.txt', avg_list, fmt='%d')
 
-    calculateCoordinates(0)
+    calculate_coordinates(0)
