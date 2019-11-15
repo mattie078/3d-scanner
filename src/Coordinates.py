0523836680fafb2abf0e682bb1e10858c8d45f6c
Matthijs Meijerink <0962038@hr.nl>
2019-11-15 15:13:30 +0100

    Poging om Coordinates te optimaliseren (grootse tijd kost in np.where)

--- src/Coordinates.py
+++ src/Coordinates.py
@@ -9,15 +9,23 @@ class Coordinates:
     def calculate_coordinates(self):
         platform_pixel_y_waarde = 570
         for i in range(73):
+            start_time = time.clock()
             # Pakt de fotos die net zijn gemaakt
             path = "..\\FotosHSV\\filter" + str(i) + ".jpg"
             new_image = cv2.imread(path)
 
             # Pakt alle coordinaten waarop de foto de punten wit zijn
             indices = np.where(new_image == [255])
+            # indices2 = []
+            # for x in range(0,new_image.shape[0]):
+            #     for y in range(0,new_image.shape[1]):
+            #         if new_image[x,y][1]==255:
+            #             indices2[0].extend(x) 
+            #             indices2[1].extend(y) 
 
             # Pakt alle coordinaten en zet ze in een list
             coordinates = list(zip(indices[0], indices[1]))
+            # coordinates2 = list(zip(indices2[0], indices2[1]))
 
             # Pakt alle coordinates en verwijderd alle duplicates
             coordinates_no_dupes = []
@@ -27,6 +35,8 @@ class Coordinates:
             zipped_list = coordinates_no_dupes[:]
 
             df = pd.DataFrame(zipped_list, columns={'yVal', 'xVal'})
+            print("1--- %s seconds ---" % (time.clock() - start_time)) 
+            start_time = time.clock()
 
             # Haalt alle unieke Y waardes uit de dataframe en slaat ze op in een nieuwe 
             df2 = df["yVal"]
@@ -36,12 +46,14 @@ class Coordinates:
             
             # Deze loop haalt alle X waardes uit de dataframe die bij een bepaalde Y waarde hoort (X type = dataframe)
             avg_list = []
+            print("2--- %s seconds ---" % (time.clock() - start_time)) 
+            start_time = time.clock()
 
             for index_old, non_duplicated_yVal in df2.iteritems():
                 val = df.loc[df["yVal"] == non_duplicated_yVal, "xVal"]
 
                 # val.real returned 'normale' array inplaats van een dataframe (voor makkelijkere verwerking)
-                val_np = np.array(val.real)
+                val_np = val.to_numpy().real
                 if val_np.size != 0:
                     # alleen waardes aan de linkerkant van de laser in het midden moeten meegenomen worden.
                     # Als een x-waarde hoger is dan die van de middenlijn betekent dit dat de laser op deze y-waarde
@@ -54,3 +66,4 @@ class Coordinates:
 
             print("Finished calc:"+str(i))
             np.savetxt('..\\temps\\calcs\\tempCalc' + str(i) + '.txt', avg_list, fmt='%d')
+            print("3--- %s seconds ---" % (time.clock() - start_time)) 
--- src/Main.py
+++ src/Main.py
@@ -1,14 +1,18 @@
 from Coordinates import Coordinates
 from PixelsToMm import PixelsToMm
 from Integratie import Integratie
+import time
+
 #from Motor import Motor
 
 def main():
     #MotorObject = Motor()
     
-    #Coordinates.calculate_coordinates(0)
+    Coordinates.calculate_coordinates(0)
     PixelsToMm.calculate_real_distances(0)
     Integratie.ReadFile(0)
 
 if __name__ == "__main__":
+    start_time = time.clock()
     main()
+    print("--- %s seconds ---" % (time.clock() - start_time)) 
