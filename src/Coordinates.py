920a9d7c1bb9661486fda3093a40dc9ded15a501
Matthijs Meijerink <0962038@hr.nl>
2019-11-25 10:16:54 +0100

    Code 5x sneller gemaakt

--- src/Coordinates.py
+++ src/Coordinates.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import pandas as pd
-import time
 
 
 class Coordinates:
@@ -10,34 +9,37 @@ class Coordinates:
         # De de afstand tussen de bovenkant van de foto en de voorkant van het platform (in pixels)
         platform_pixel_y_waarde = 570
         for i in range(73):
-            start_time = time.clock()
             # Pakt de fotos die net zijn gemaakt
             path = "..\\FotosHSV\\filter" + str(i) + ".jpg"
             new_image = cv2.imread(path)
 
             # Pakt alle coordinaten waarop de foto de punten wit zijn
             indices = np.where(new_image == [255])
-            # indices2 = []
-            # for x in range(0,new_image.shape[0]):
-            #     for y in range(0,new_image.shape[1]):
-            #         if new_image[x,y][1]==255:
-            #             indices2[0].extend(x) 
-            #             indices2[1].extend(y) 
 
             # Pakt alle coordinaten en zet ze in een list
             coordinates = list(zip(indices[0], indices[1]))
             # coordinates2 = list(zip(indices2[0], indices2[1]))
 
             # Pakt alle coordinates en verwijderd alle duplicates
-            coordinates_no_dupes = []
-            for index in coordinates:
-                if index not in coordinates_no_dupes:
-                    coordinates_no_dupes.append(index)
-            zipped_list = coordinates_no_dupes[:]
+            coordinates_no_dupes1 = []
+            # coordinates_no_dupes = []
 
+            setA = set(map(tuple, coordinates))
+            setB = set(map(tuple, coordinates_no_dupes1))
+
+            for o in setA: 
+                if o not in setB:
+                    coordinates_no_dupes1.append(o)
+
+            # Loop
+            # for index in coordinates:
+            #     if index not in coordinates_no_dupes:
+            #         coordinates_no_dupes.append(index)
+            
+
+            zipped_list = coordinates_no_dupes1[:]
             df = pd.DataFrame(zipped_list, columns={'yVal', 'xVal'})
-            print("1--- %s seconds ---" % (time.clock() - start_time)) 
-            start_time = time.clock()
+
 
             # Haalt alle unieke Y waardes uit de dataframe en slaat ze op in een nieuwe 
             df2 = df["yVal"]
@@ -47,8 +49,6 @@ class Coordinates:
             
             # Deze loop haalt alle X waardes uit de dataframe die bij een bepaalde Y waarde hoort (X type = dataframe)
             avg_list = []
-            print("2--- %s seconds ---" % (time.clock() - start_time)) 
-            start_time = time.clock()
 
             for index_old, non_duplicated_yVal in df2.iteritems():
                 val = df.loc[df["yVal"] == non_duplicated_yVal, "xVal"]
@@ -67,4 +67,3 @@ class Coordinates:
 
             print("Finished calc:"+str(i))
             np.savetxt('..\\temps\\calcs\\tempCalc' + str(i) + '.txt', avg_list, fmt='%d')
-            print("3--- %s seconds ---" % (time.clock() - start_time)) 
