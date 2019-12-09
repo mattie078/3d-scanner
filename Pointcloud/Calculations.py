9c2c70e6957a6519825d457c1b1ede501a83bd13
Daan <daanvanschie@live.nl>
2019-12-09 21:43:56 +0100

    cartesian to bol fixed

--- Pointcloud/Calculations.py
+++ Pointcloud/Calculations.py
@@ -2,22 +2,33 @@ import numpy as np
 import math as math
 class Calculations:
     angle = 12
+    rotations = 73
 
-    def calculateHypothenuse(self):
-        for x in range(73):
-            path = '..\\temps\\xycoordinates\\tempXY' + str(x) + '.txt'
-            getmm_array = np.loadtxt(fname=path, dtype='float')
-            # print(getmm_array)
+    def calculateHypothenuse(self, x):
+        path = '..\\temps\\xycoordinates\\tempXY' + str(x) + '.txt'
+        getmm_array = np.loadtxt(fname=path, dtype='float')
 
-            for y in range(0,getmm_array.shape[0]):
-                
-                getmm_array[y][0]=getmm_array[y][0]/math.sin(12*(math.pi/180))
-                # print(getmm_array[y][0])
-                variabel + int(y) = getmm_array[y][0]
-                print(variabel +int(y))
+        for y in range(0,getmm_array.shape[0]):                
+            getmm_array[y][0]=getmm_array[y][0]/math.sin(12*(math.pi/180))  #12 veranderen naar angle
+
+        return getmm_array
+
+    def cartesianToSpherical(self, hypo_array, x):
+        for y in range(0,hypo_array.shape[0]):
+            polarangle = math.atan(hypo_array[y][1]/hypo_array[y][0])/(math.pi/180) #90-angle of angle
+            radius = math.sqrt(math.pow(hypo_array[y][1], 2) + math.pow(hypo_array[y][0], 2))
+            motorAngle = (360/73*x) # checken voor draaifoto volgorde
+            calc.SphericalToCartesian(polarangle, radius, motorAngle)
+
+    def SphericalToCartesian(self, polarangle. radius, motorAngle):
+        xcoordinate = radius*math.sin(90-polarangle)*math.cos(motorAngle)
 
 calc = Calculations()
-calc.calculateHypothenuse()
+
+for x in range(73): #73 later nog veranderen in rotations
+    hypo_array = calc.calculateHypothenuse(x)
+    calc.cartesianToSpherical(hypo_array, x)
+    # print(hypo_array)
 
 
 
