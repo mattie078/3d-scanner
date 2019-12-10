1b97b60fc6e1f39482f7a301324061129397a371
Herman Heringa <hermanheringa@hotmail.com>
2019-12-10 23:18:19 +0100

    comment toegevoegd in main

--- src/Main.py
+++ src/Main.py
@@ -15,6 +15,6 @@ def main():
     Integratie.ReadFile(0)
 
 if __name__ == "__main__":
-    start_time = time.perf_counter()
+    start_time = time.perf_counter()    # .clock() mag niet meer gebruikt te worden sinds Python3.3. dit doet hetzelfde.
     main()
     print("--- %s seconds ---" % (time.perf_counter() - start_time))
