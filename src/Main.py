bd20f9877a543f9a0ecc884474ef2d796f94bf77
Matthijs Meijerink <0962038@hr.nl>
2019-12-18 15:25:00 +0100

    LED test

--- src/Main.py
+++ src/Main.py
@@ -6,14 +6,17 @@ import RPi.GPIO as GPIO
 import cv2
 
 buttonPin = 10
-ledPin = 12
+
+pinRood = 36
+pinBlauw = 38
+pinGroen = 40
 
 def waitForInput():
     GPIO.output(ledPin, GPIO.HIGH) #GROEN
-    print("Waiting on initial input...")
+    #print("Waiting on initial input...")
     while True:
         if GPIO.input(buttonPin) == GPIO.LOW:
-            print("Ingedrukt!")
+            #print("Knop is ingedrukt!")
             GPIO.output(ledPin, GPIO.HIGH) #ROOD
             break   # Knop is ingedrukt
 
@@ -38,7 +41,25 @@ if __name__ == "__main__":
 
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(ledPin, GPIO.OUT, initial=GPIO.LOW)
+
+    GPIO.setup(pinRood, GPIO.OUT)
+    GPIO.setup(pinGroen, GPIO.OUT)
+    GPIO.setup(pinBlauw, GPIO.OUT)
+
+    ROOD = GPIO.PWM(pinRood, 1000)
+    GROEN = GPIO.PWM(pinGroen, 1000)
+    BLAUW = GPIO.PWM(pinBlauw, 1000)
+    ROOD.start(0)
+    GROEN.start(0)
+    BLAUW.start(0)
+
+    roodwaarde = (255 * 100) / 255
+    groenwaarde = (0 * 100) / 255
+    blauwwaarde = (0 * 100) / 255
+ 
+    ROOD.ChangeDutyCycle(roodwaarde)
+    GROEN.ChangeDutyCycle(groenwaarde)
+    BLAUW.ChangeDutyCycle(blauwwaarde)
 
     main()
 
