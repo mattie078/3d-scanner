cc6ede4cb15543837ca7bac36dafd7241904baef
Matthijs Meijerink <0962038@hr.nl>
2019-12-20 09:55:45 +0100

    Autocommit

--- src/motor.py
+++ src/motor.py
@@ -5,31 +5,28 @@ from datetime import datetime
 from Filter import Filter
 
 class Motor:
-    
-    def __init__(self): 
 
-            # Opent de camera om een foto te makenn
-            camera = PiCamera()
-            GPIO.setmode(GPIO.BOARD)
-            ControlPin = [6,13,19,26]
-            filterImage = Filter()
-            # for pin in ControlPin:
-            #     GPIO.setup(pin,GPIO.OUT)
-            #     GPIO.output(pin,0)
+    camera = PiCamera()
+    GPIO.setmode(GPIO.BOARD)
+    ControlPin = [6,13,19,26]
+    filterImage = Filter()
+    # for pin in ControlPin:
+    #     GPIO.setup(pin,GPIO.OUT)
+    #     GPIO.output(pin,0)
 
-            # Sequence om de motor juist te laten draaien
-            seq = [[1,0,0,0],
-                [1,1,0,0],
-                [0,1,0,0],
-                [0,1,1,0],
-                [0,0,1,0],
-                [0,0,1,1],
-                [0,0,0,1],
-                [1,0,0,1],
-                [1,0,0,0]]
+    # Sequence om de motor juist te laten draaien
+    seq = [[1,0,0,0],
+        [1,1,0,0],
+        [0,1,0,0],
+        [0,1,1,0],
+        [0,0,1,0],
+        [0,0,1,1],
+        [0,0,0,1],
+        [1,0,0,1],
+        [1,0,0,0]]
 
-            # Maakt 73 fotos (0 tot 72) en slaat deze op met de juiste benaming
-            # Zet de filter op de foto
+    # Maakt 73 fotos (0 tot 72) en slaat deze op met de juiste benaming
+    # Zet de filter op de foto
 
     def turnMotor(self, i):
         fotonaam = 'temp' + str(i) + ".jpg"
