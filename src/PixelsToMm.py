17d9be634c45c6fa5a6a7cceee3db21e90e837f0
Matthijs Meijerink <0962038@hr.nl>
2020-01-08 12:04:26 +0100

    Autocommit

--- src/PixelsToMm.py
+++ src/PixelsToMm.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class PixelsToMm:
 
-    def calculate_real_distances(self):
+    def calculate_real_distances(self, i, tempList):
         # Formule van de kalibratie: 
         # pixels = (6,3933* mm) - 10,338
 
@@ -13,27 +13,25 @@ class PixelsToMm:
         a = 6.3933
         b = -10.338
         hoek = 12
-        platform_pixel_y_waarde = 570   # opgemeten uit de verschillende foto's die we genomen hebben. Zat marge op.
+        # platform_pixel_y_waarde = 570 # opgemeten uit de verschillende foto's die we genomen hebben. Zat marge op.
+                                        # NIET MEER NODIG
+        # Pakt de lijnbreedtes per y-coordinaat die net zijn berekend
+        data_array = np.array(tempList, dtype='int')
 
-        for i in range(73):
-            # Pakt de lijnbreedtes per y-coordinaat die net zijn berekend
-            path = '..\\temps\\calcs\\tempCalc'+str(i)+'.txt'
-            data_array = np.loadtxt(fname=path, dtype='int')
+        end_array = []
+        # De actuele milimeters voor elke waarde in tempCalc berekenen
+        for j in range(0, data_array.shape[0]):
+            y_waarde = data_array[j][0]
+            # pixel_hoogte ging eerst fout, dit is hoe het zou moeten. (zie xyCalcsAfbeelding2.png)
+            pixel_hoogte = y_waarde
+            pixel_breedte = data_array[j][1]
 
-            end_array = []
-            # De actuele milimeters voor elke waarde in tempCalc berekenen
-            for j in range(0, data_array.shape[0]):
-                y_waarde = data_array[j][0]
-                # pixel_hoogte ging eerst fout, dit is hoe het zou moeten. (zie xyCalcsAfbeelding2.png)
-                pixel_hoogte = platform_pixel_y_waarde - y_waarde
-                pixel_breedte = data_array[j][1]
+            if pixel_breedte < 250: # groot schepnet tegen ruis
 
-                if pixel_breedte < 250: # groot schepnet tegen ruis
+                mm_hoogte = ((pixel_hoogte - b) / a)
+                mm_breedte = ((pixel_breedte - b) / a)
 
-                    mm_hoogte = ((pixel_hoogte - b) / a)
-                    mm_breedte = ((pixel_breedte - b) / a)
+                end_array.append([mm_breedte, mm_hoogte])
 
-                    end_array.append([mm_breedte, mm_hoogte])
-
-            print("Finished xycoordinates:"+str(i))
-            np.savetxt('..\\temps\\xycoordinates\\tempXY' + str(i) + '.txt', end_array, fmt='%f')
+        print("Finished xycoordinates:"+str(i))
+        np.savetxt('..\\temps\\xycoordinates\\tempXY' + str(i) + '.txt', end_array, fmt='%f')
