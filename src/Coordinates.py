4a90325e1282425ecbc23f60d7fb410d9204bcf2
Matthijs Meijerink <0962038@hr.nl>
2020-01-08 11:57:42 +0100

    Autocommit

--- src/Coordinates.py
+++ src/Coordinates.py
@@ -2,21 +2,19 @@ import cv2
 import numpy as np
 import pandas as pd
 from PixelsToMm import PixelsToMm
-import os 
-
+import os
 
 
 class Coordinates:
 
-def calculate_coordinates(self, i):
+    def calculate_coordinates(self, i):
         # De de afstand tussen de bovenkant van de foto en de voorkant van het platform (in pixels)
         # platform_pixel_y_waarde = 570 NIET MEER NODIG
         # Pakt de fotos die net zijn gemaakt
-	dir_path = os.path.dirname(os.path.realpath(__file__))
-	print(dir_path)
-	path = dir_path + "\\..\\FotosHSV\\filter" + str(i) + ".jpg"
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        print(dir_path)
+        path = dir_path + "\\..\\FotosHSV\\filter" + str(i) + ".jpg"
         new_image = cv2.imread(path)
-
 
         # Pakt alle coordinaten waarop de foto de punten wit zijn
         indices = np.where(new_image == [255])
@@ -41,10 +39,8 @@ def calculate_coordinates(self, i):
         #     if index not in coordinates_no_dupes:
         #         coordinates_no_dupes.append(index)
 
-
         zipped_list = coordinates_no_dupes1[:]
         df = pd.DataFrame(zipped_list, columns={'yVal', 'xVal'})
-
 
         # Haalt alle unieke Y waardes uit de dataframe en slaat ze op in een nieuwe
         df2 = df["yVal"]
@@ -66,14 +62,16 @@ def calculate_coordinates(self, i):
                 # achter het middelpunt van het draaivlak schijnt en er dus geen voorwerp tussen staat op die
                 # hoogte, oftewel: het voorwerp bevindt zich onder deze y-waarde en deze punten moeten dus
                 # genegeerd worden. (zie Documenten/xyCalcsAfbeelding1.png)
-                if val_np.max(axis=0) < 1120:  # X-waarde van de rechter laser. Opgemeten uit foto's.
+                # X-waarde van de rechter laser. Opgemeten uit foto's.
+                if val_np.max(axis=0) < 1120:
                     length = val_np.max(axis=0)-val_np.min(axis=0)
                     avg_list.append([non_duplicated_yVal, length])
 
         # print("Finished calc:"+str(i))  # NIET MEER NODIG
         # np.savetxt('..\\temps\\calcs\\tempCalc' + str(i) + '.txt', avg_list, fmt='%d')
-        #PixelsToMm.calculate_real_distances(0, i, avg_list)   # linkt nu direct door.
+        # PixelsToMm.calculate_real_distances(0, i, avg_list)   # linkt nu direct door.
         # Bespaart tijd omdat we nu niet meer naar de harde schijf lezen/schrijven, alles blijft in RAM.
+
 
 co = Coordinates()
 co.calculate_coordinates(1)
