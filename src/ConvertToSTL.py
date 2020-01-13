47a4cd39da15f1415dbf9b9dc4b2f801dc7bec29
Matthijs Meijerink <matthijsmeijerink97@gmail.com>
2020-01-13 14:45:02 +0100

    kleine edit

--- src/ConvertToSTL.py
+++ src/ConvertToSTL.py
@@ -4,7 +4,7 @@ class MeshlabTask():
 
     def run():
         mlx_script_path = "default_meshing.mlx"
-        input = "../temps/pointCloudTest.ply"
+        input = "../temps/pointCloud.ply"
         output = "../temps/STLFILE.stl"
         os.system("xvfb-run meshlabserver -i " + input + " -o " + output + " -s " + mlx_script_path + " -om vc vn > tmp")
         print (open('tmp', 'r').read())
