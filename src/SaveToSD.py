ab1d2e1afb06b839817046612f985fc7d60fdab0
Matthijs Meijerink <matthijsmeijerink97@gmail.com>
2020-01-13 11:15:04 +0100

    Saveing to SD

--- src/SaveToSD.py
+++ src/SaveToSD.py
@@ -9,13 +9,17 @@ class SaveToSD():
         dir_path = os.path.dirname(os.path.realpath(__file__))
     
         src_path = dir_path + "/../temps/STLFILE.stl" # Naam van STL bestand
-        dst_path = dir_path + "/../../../media/pi" # Als het goed is komt hier de SD kaart te staan
-
+        dst_path = dir_path + "/media/pi" # Als het goed is komt hier de SD kaart te staan
+	print(str(dst_path))
         # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory?rq=1
         directories = []
-        for (os.dirpath, os.dirnames, os.filenames) in os.walk(path):
+        for (os.dirpath, os.dirnames, os.filenames) in os.walk(dir_path):
             directories.extend(os.filenames)   # Voegt namen toe aan de array
             break
+	dst_path = dst_path + str(directories[0] + datetime.now().strftime('STL-%d-%m-%Y-%H:%M:%S') + '.stl') # Waarschijnlijk maar 1 resultaat, moeten we testen
 
-        dst_path = dst_path + str(directories[0] + datetime.now().strftime('STL-%d-%m-%Y-%H:%M:%S') + '.stl') # Waarschijnlijk maar 1 resultaat, moeten we testen
-        shutil.copy(src_path,dst_path)
+	print(str(dst_path))
+	#shutil.copy(src_path,dst_path)
+
+s = SaveToSD() 
+s.save()
